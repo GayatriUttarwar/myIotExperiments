9d836fe51693262501c9dd1147a61e6124f5d1e4
GayatriUttarwar <36923873+GayatriUttarwar@users.noreply.github.com>
2018-02-28 19:11:03 +0530

    Add files via upload

---
+++ time_travel.py
@@ -0,0 +1,9 @@
+import time
+import random
+a = input('Enter your name')
+if a == 'asdf':
+     print 'you learned string comparison'
+for i in range(10):
+     b = random.randint(0,10)
+     print b
+     time.sleep(0.5)
